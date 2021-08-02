048f6194e10fce8c8690d3a5a76c2c57547ef26d
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-08-02 19:45:36 -0300

    Implementado funções do menu Attach (galeria, fotos, documentos e contatos)

--- src/controller/WhatsAppConreoller.js
+++ src/controller/WhatsAppConreoller.js
@@ -127,13 +127,13 @@ class WhatsAppConreoller {
             console.log(this.el.inputNamePanelEditProfile.innerHTML);
         });
 
-        this.formPanelAddContact.on('submit', e => {
+        this.el.formPanelAddContact.on('submit', e => {
             e.preventDefault();
             let formData = new FormData(this.formPanelAddContact);
         });
 
         this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
-            item.on('click', e=>{
+            item.on('click', e => {
                 this.el.home.hide();
                 this.el.main.css({
                     display: 'flex'
@@ -141,6 +141,80 @@ class WhatsAppConreoller {
             });
         });
 
+        
+        this.el.btnAttach.on('click', e => {
+            e.stopPropagation();
+            this.el.menuAttach.addClass('open');
+            document.addEventListener('click', this.closeMenuAttach.bind(this));
+        });
+
+        this.el.btnAttachPhoto.on('click', e => {
+            this.el.inputPhoto.click();
+        });
+
+        this.el.inputPhoto.on('change', e =>{
+            console.log(this.el.inputPhoto.files);
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
+        });
+
+        this.el.btnAttachCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                'height': 'calc(100% - 120px)'
+            });
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e => {
+            console.log('Tirei uma foto');
+        });
+
+        this.el.btnAttachDocument.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                'height': '100%'
+            });
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on('click', e=>{
+            console.log('documento enviado');
+        });
+
+        this.el.btnAttachContact.on('click', e => {
+            this.el.modalContacts.show();
+        });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+
+
+    }
+
+    closeAllMainPanel(){
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
+    }
+
+    //erro de propagação
+    closeMenuAttach(e){
+        document.removeEventListener('click', this.closeMenuAttach);
+        this.el.menuAttach.removeClass('open');
+        console.log('remove menu');
     }
 
     closeAllLeftPanel() {
