996df3c53f07a6348f6c8553b97bc0f3396c5bae
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-08-02 17:58:56 -0300

    Implementando a classe WhatsAppController.js

--- src/controller/WhatsAppConreoller.js
+++ src/controller/WhatsAppConreoller.js
@@ -1,4 +1,112 @@
 class WhatsAppConreoller {
 
-    
+    constructor() {
+
+        this.elementsPrototype();
+        this.loadElements();
+        this.iniEvents();
+
+    }
+
+    loadElements() {
+
+        this.el = {};
+        document.querySelectorAll('[id]').forEach(element => {
+            this.el[Format.getCamelCase(element.id)] = element;
+        });
+
+    }
+
+    elementsPrototype() {
+
+        Element.prototype.hide = function (params) {
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function (params) {
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function (params) {
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function (events, fn) {
+            events.split(' ').forEach(event => {
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+
+        Element.prototype.css = function (styles) {
+            for (let name in styles) {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+        
+        Element.prototype.addClass = function (name) {
+            this.classList.add(name);
+            return this;
+        }
+
+        Element.prototype.removeClass = function (name) {
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function (name) {
+            this.classList.toggle(name);
+            return this;
+        }
+
+        Element.prototype.hasClass = function (name) {
+            return this.classList.contains(name);
+        }
+    }
+
+    iniEvents() {
+
+        this.el.myPhoto.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 50);
+
+        });
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 50);
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelEditProfile.hide();
+        this.el.panelAddContact.hide();
+    }
+
+
 }
