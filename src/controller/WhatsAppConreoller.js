e4261205beba347568400ded99834be7da347576
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-08-02 18:15:48 -0300

    implementado novas alterações sobre a área de PanelEditProfile e PanelAddContact

--- src/controller/WhatsAppConreoller.js
+++ src/controller/WhatsAppConreoller.js
@@ -47,7 +47,7 @@ class WhatsAppConreoller {
             }
             return this;
         }
-        
+
         Element.prototype.addClass = function (name) {
             this.classList.add(name);
             return this;
@@ -99,6 +99,22 @@ class WhatsAppConreoller {
 
         this.el.photoContainerEditProfile.on('click', e => {
             this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+            if(e.key === 'Enter'){
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        });
+
+        this.formPanelAddContact.on('submit', e => {
+            e.preventDefault();
+            let formData = new FormData(this.formPanelAddContact);
         });
 
     }
