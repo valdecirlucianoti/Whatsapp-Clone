a2f337f9999faad2935a66d8dd5e479d04225b74
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-08-19 19:14:42 -0300

    Implementado as configurações de troca de foto de perfil

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,5 +1,6 @@
 import { Firebase } from "../util/Firebase";
 import { Format } from "../util/Format";
+import { Upload } from "../util/Upload";
 import { Model } from "./Model";
 
 export class Message extends Model {
@@ -392,29 +393,7 @@ export class Message extends Model {
 
     static upload(file, from) {
 
-        return new Promise(() => {
-
-            let uploadTask = Firebase
-                .hd()
-                .ref(from)
-                .child(Date.now() + '_' + file.name)
-                .put(file);
-
-            uploadTask.on('state_changed', e => {
-
-                console.info('upload', e);
-
-            }, err => {
-
-                f(err);
-
-            }, () => {
-
-                s(uploadTask.snapshot);
-
-            });
-
-        });
+        return Upload.send(file, from);
 
     }
 
